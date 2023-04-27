e48e1fc29104ebe97ebfe2706ddec58b16613a2e
HyeonWooGa <yeonwoopark22@gmail.com>
2023-04-27 23:09:03 +0900

    📝: final, const

--- main.dart
+++ main.dart
@@ -1,27 +1,10 @@
 void main() {
-  late var anything; // dynamic
-  /*
-  print(anything);  // Error
-  */
-  anything = true;
-  print(anything); // OK
-  anything = 3;
+  // Final: JavaScript 의 const 키워드와 동일한 동작
+  // Const: 컴파일 시 고정된 값을 가지는 변수일 떄 사용 ex) api 주소, 최댓값 등
 
-  late String name; // String
-  /*
-  print(name);  // Error
-  */
-  name = 'park';
-  print(name); // OK
-  anything = 'lee';
+  final String name = '홍길동';
+  const int maxCount = 10;
 
-  late final double height;
-  /*
-  print(height); // Error
-  */
-  height = 167.8;
-  print(height); // OK
-  /*
-  height = 167.9; // Error
-  */
+  // name = '김철수'; // Error: The final variable 'name' can only be set once.
+  // maxCount = 5; // Error: The final variable 'maxCount' can only be set once.
 }
