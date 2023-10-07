b371dff8439bada8673d00f7b6a2dcbb6aa5b488
HyeonWooGa <yeonwoopark22@gmail.com>
2023-10-08 04:16:39 +0900

    📝: Sets

--- main.dart
+++ main.dart
@@ -1,56 +1,19 @@
-// Maps
+// Sets
+//// Set 은 List 와 비슷하지만 중괄호를 사용하고
+//// 요소의 중복을 허용하지 않는다.
 
 void main() {
-  // var 키워드 이용, 자료형 추론 사용되어 Map 생성
-  var player1 = {
-    'name': 'park',
-    'xp': 19.99,
-    'superpower': true,
-  };
-  // 자료형은 Map<String, Object>
-  // Object 는 타입스크립트의 any 와 같은 역할을 한다.
-  // 다트는 모든 것이 Object 즉 객체로 이루어져 있기 때문에
+  var numbers1 = {1, 2, 3, 4};
+  Set<int> numbers2 = {1, 2, 3, 4};
 
-  print(player1);
+  print(numbers1); // {1, 2, 3, 4} 출력
+  print(numbers2); // {1, 2, 3, 4} 출력
 
-  // Map 키워드 이용, 자료형 지정하여 Map 생성
-  Map<String, Object> player2 = {
-    'name': 'lee',
-    'xp': 19.99,
-    'superpower': true,
-  };
+  numbers1.add(5);
+  print(numbers1); // {1, 2, 3, 4, 5} 출력
 
-  print(player2);
-
-  // 아래는 우리가 키, 밸류 값으로 상당히 복잡한 자료형을 넣을 수 있다는 예시
-  Map<List<int>, List<String>> example1 = {
-    [1, 2]: ['park', 'lee'],
-    [3, 4]: ['kim', 'lee'],
-  };
-
-  print(example1);
-
-  Map<Object, Object> example2 = {
-    [1, 2]: ['park', 'lee'],
-    [3, 4]: ['kim', 'lee'],
-  };
-
-  print(example2);
-
-  var players1 = [
-    player1,
-    player2,
-  ];
-
-  List<Map<String, Object>> players2 = [
-    player1,
-    player2,
-  ];
-
-  print(players1);
-  print(players2);
+  numbers1.add(5);
+  numbers1.add(5);
+  numbers1.add(5);
+  print(numbers1); // {1, 2, 3, 4, 5} 출력
 }
-
-// 다만 자바스크립트에서 같이 API 를 처리하는데 
-// 굳이 자바스크립트에서 Object 역할을 하는 다트의 Maps 를 사용하지 않고 
-// 다양하고 유용한 기능이 많은 다트의 Class 를 추천
