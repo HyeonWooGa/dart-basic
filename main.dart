4f24b6a4a7ebd96fb4265f821f04dad515447e7a
HyeonWooGa <yeonwoopark22@gmail.com>
2023-10-08 19:07:58 +0900

    📝: First Dart Class

--- main.dart
+++ main.dart
@@ -1,44 +1,35 @@
-// Typedef
-//// 자료형이 헷갈릴 때 도움이 될 자료형 Alias(별명) 를 만드는 방법
-//// 기본적으로 typedef 를 TS 처럼 코드 최상단이나 Typedefs 파일에
-//// 한 눈에 보이게 사용하면 좋을 것 같습니다.
-//// 현재는 학습 중이므로 코드 중간중간에 사용했습니다.
+// First Dart Class (생성자 아직 사용 X)
+//// Class Property 정의 시에 자료형을 지정해줘야 한다.
 
-List<int> reverseListOfNumbers1(List<int> numbers) {
-  var reversed = numbers.reversed; // Iterable
-  return reversed.toList(); // List
+////// Class 정의
+class Player {
+  //Properties
+  String name = 'Lee';
+  final String id = 'ER23YT'; // Class 의 프로퍼티가 수정 불가능 (final 키워드)
+  int xp = 1500;
+
+  //Method
+  void sayHello() {
+    print('Hello I\'m $name($id)');
+    // this.name 은 Mathod 내 변수 이름과
+    // Class 내 변수 이름이 겹치는 경우에만
+    // Class 내 변수를 호출하기 위해 사용
+    // 일반적으로 this.name 과 같이 Property 호출하는 것은 지양
+  }
 }
-
-typedef ListOfInts = List<int>; // ListOfInts == List<int>
-
-ListOfInts reverseListOfNumbers2(ListOfInts numbers) =>
-    numbers.reversed.toList();
-
-typedef SetOfInts = Set<int>;
-
-SetOfInts addFiveToSetOfNumbers(SetOfInts numbers) {
-  numbers.add(5);
-
-  return numbers;
-}
-
-typedef MapOfCouple = Map<String, String>;
-
-String introduceCouple(MapOfCouple couple) =>
-    '${couple['man']} and ${couple['woman']} are a very beautiful couple.';
 
 void main() {
-  ListOfInts list = [1, 2, 3, 4];
-  SetOfInts set = {1, 2, 3, 4};
-  MapOfCouple map = {
-    'man': 'Park',
-    'woman': 'Lee',
-  };
+  ////// 인스턴스 생성
+  var player = Player(); // var player = new Player(); new 는 생략 가능
+  print('Name: ${player.name}, Id: ${player.id}');
+  // 'Name: Lee, Id: ER23YT' 출력
 
-  print(reverseListOfNumbers1(list)); // [4,3,2,1] 출력
-  print(reverseListOfNumbers2(list)); // [4,3,2,1] 출력
+  player.sayHello(); // 'Hello I'm Lee(ER23YT)' 출력
 
-  print(addFiveToSetOfNumbers(set)); // {1, 2, 3, 4, 5} 출력
+  player.name = 'Park';
+  // player.id = 'ET87TY';   // Error 발생
+  print('Name: ${player.name}, Id: ${player.id}');
+  // 'Name: Park, Id: ER23YT' 출력
 
-  print(introduceCouple(map)); // 'Park and Lee are a very beautiful couple.'
+  player.sayHello(); // 'Hello I'm Park(ER23YT)' 출력
 }
