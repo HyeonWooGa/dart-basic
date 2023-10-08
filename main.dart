24cda77dded3f6350f787e979f62a5abf0dde7a2
HyeonWooGa <yeonwoopark22@gmail.com>
2023-10-08 16:02:47 +0900

    📝: QQ Operator & QQ Equls

--- main.dart
+++ main.dart
@@ -1,13 +1,37 @@
-// Optional Positional Parameters
-//// Positional Parameters 에 초기값을 주어 함수 호출시 전달인자를 생략할 수 있는 방법
-//// 굳이 사용되지 않고 Named Parameters 를 사용하는 것이 더 권장되어 보임
+// QQ Operator & QQ Equals(=QQ Assignment Operator)
 
-String sayHello(String personName, String myName,
-    [int? myAge = 99, String? myCountry = 'Earth']) {
-  return 'Hello $personName, I\'m $myName, $myAge and from $myCountry.';
+//// 함수 선언식
+String capitalizeName1(String? name) {
+  if (name != null) {
+    return name.toUpperCase();
+  }
+  return 'ANON';
+}
+
+//// 화살표 함수 + 삼항 연산자
+String capitalizeName2(String? name) =>
+    name != null ? name.toUpperCase() : 'ANON';
+
+//// 화살표 함수 + QQ 연산자
+String capitalizeName3(String? name) => name?.toUpperCase() ?? 'ANON';
+
+//// QQ Equals
+String capitalizeName4(String? name) {
+  name ??= 'anon';
+
+  return name.toUpperCase();
 }
 
 void main() {
-  print(sayHello('Lee', 'Park', 31));
-  // 'Hello Lee, I'm Park, 31 and from Earth.' 출력
+  print(capitalizeName1('Park')); // 'PARK' 출력
+  print(capitalizeName1(null)); // 'ANON' 출력
+
+  print(capitalizeName2('Lee')); // 'LEE' 출력
+  print(capitalizeName2(null)); // 'ANON' 출력
+
+  print(capitalizeName3('Shin')); // 'SHIN' 출력
+  print(capitalizeName3(null)); // 'ANON' 출력
+
+  print(capitalizeName4('Kang')); // 'KANG' 출력
+  print(capitalizeName4(null)); // 'ANON' 출력
 }
