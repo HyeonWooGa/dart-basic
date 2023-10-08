3e27a80a38dcd4eaba4ac2d4d5cfc8f18785e8bc
HyeonWooGa <yeonwoopark22@gmail.com>
2023-10-09 00:25:47 +0900

    📝: Named Constructor

--- main.dart
+++ main.dart
@@ -1,11 +1,4 @@
 // Named Constructor Parameters
-//// 함수의 Positinal Parameters 와 마찬가지로
-//// 생성자의 Parameters 를 Positional Parameters 로
-//// 사용하면 Parameters 가 많아졌을 때 문제가 생겨서
-//// Named Parameters 를 쓰는 것이 좋다.
-//// Named Parameters 의 Null Safety 관련 에러는
-//// required 를 쓰는 것이 대부분 좋다.
-//// 하지만 특별한 경우에는 초깃값 설정으로 쓸 일도 많을듯!
 
 //// Class 정의
 class Player {
@@ -21,18 +14,53 @@ class Player {
     required this.team,
   });
 
+  // Named Constructor; Named Constructor Parameters 사용
+  Player.createChristiansPlayer({
+    required this.name,
+    required this.id,
+    this.xp = 0,
+    this.team = 'Christians',
+  });
+
+  // Named Constructor; Named Constructor Parameters & 콜론 사용
+  Player.createNonChristianPlayer({
+    required String name,
+    required String id,
+  })  : this.name = name,
+        this.id = id,
+        this.team = 'Non Christians',
+        this.xp = 0;
+
+  // Named Constructor; Positional Constructor Parameters 사용
+  Player.createUnknownPlayer(this.name, this.id,
+      [this.xp = 0, this.team = 'Unknown']);
+
   void sayHello() {
-    print('Hello I\'m $name($id) from $team');
+    print('Hello I\'m $name($id) from $team team');
   }
 }
 
 void main() {
-  var player = Player(
+  // 기본 Constructor
+  var player1 = Player(
     name: 'Park',
     team: 'Christians',
     id: 'yeonwoopark22',
     xp: 1500,
   );
 
-  player.sayHello();
+  // Named Constructor; Named Constructor Parameters 사용
+  var player2 = Player.createChristiansPlayer(name: 'Lee', id: 'igh1482');
+
+  // Named Constructor; Named Constructor Parameters & 콜론 사용
+  var player3 = Player.createNonChristianPlayer(name: 'Choi', id: 'choi77');
+
+  // Named Constructor; Positional Constructor Parameters 사용
+  var player4 = Player.createUnknownPlayer('Ryu', 'rjy90');
+
+  // 각각의 생성자로 생성된 인스턴스 메소드 사용하여 Print; 모두 정상 동작
+  player1.sayHello();
+  player2.sayHello();
+  player3.sayHello();
+  player4.sayHello();
 }
