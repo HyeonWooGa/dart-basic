7a778d7b0120b89a8c210dc3ea37d9334499d8c5
HyeonWooGa <yeonwoopark22@gmail.com>
2023-10-09 01:33:19 +0900

    📝: Cascade Notation

--- main.dart
+++ main.dart
@@ -1,36 +1,16 @@
-// Recap_Class Constructor
-//// 실제 구현환경에서 우리가 API 를 통해 데이터를 전달 받는다면
-//// 우리는 그 데이터를 Dart Class 로 바꿔야한다.
-
-//가상의 Mock Data
-var apiData = [
-  {
-    'name': 'Park',
-    'team': 'Christian',
-    'xp': 0,
-  },
-  {
-    'name': 'Shin',
-    'team': 'Christian',
-    'xp': 0,
-  },
-  {
-    'name': 'Lee',
-    'team': 'Christian',
-    'xp': 0,
-  },
-];
+// Cascade Notation
+//// Dart 에서 정말 쿨한 것 중의 하나
+//// 유용한 Syntax Sugar
 
 class Player {
   int xp;
-  String team;
-  final String name;
+  String name, team;
 
-  //Flutter 앱에서 많이 사용하는 패턴
-  Player.fromJson(Map<String, dynamic> playerJson)
-      : name = playerJson['name'],
-        xp = playerJson['xp'],
-        team = playerJson['team'];
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
 
   void sayHello() {
     print('Hi my name is $name');
@@ -38,8 +18,19 @@ class Player {
 }
 
 void main() {
-  apiData.forEach((playerJson) {
-    var player = Player.fromJson(playerJson);
-    player.sayHello();
-  });
+  //인스턴스 생성직후 Cascade Notation 사용
+  var park = Player(
+    name: 'Park',
+    xp: 0,
+    team: 'Christian',
+  )
+    ..name = 'PYW'
+    ..xp = 100
+    ..sayHello(); // 'Hi my name is PYW' 출력
+
+  //인스턴스 생성직후가 아닐때 Cascade Notation 사용
+  var temp = park
+    ..name = 'Park'
+    ..xp = 1000
+    ..sayHello(); // 'Hi my name is Park' 출력
 }
